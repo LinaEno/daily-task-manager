a6e3dc4eb03f8d92e7f20951d6de2b173e1b98d6
Lina Budkova <linabudkova35@gmail.com>
2023-04-10 19:33:12 +0300

    add modal edit

--- src/components/Tasks/TasksPage.jsx
+++ src/components/Tasks/TasksPage.jsx
@@ -12,11 +12,15 @@ import {
 } from 'firebase/firestore';
 import { db } from '../../firebase';
 import { openModalEditTask } from 'redux/global/slice';
+import { selectIsModalEditTaskOpen } from 'redux/global/selectors';
+import { ModalContainer } from 'components/ModalContainer/ModalContainer';
+import ModalEditTask from 'components/ModalEditTask/ModalEditTask';
 
 const TasksPage = () => {
   const currentUserUid = useSelector(selectCurrentUserUid);
   const [tasks, setTasks] = useState([]);
   const dispatch = useDispatch();
+  const isModalOpen = useSelector(selectIsModalEditTaskOpen);
 
   useEffect(() => {
     if (!currentUserUid) return;
@@ -54,31 +58,72 @@ const TasksPage = () => {
   };
 
   return (
-    <Container>
-      <h3>Daily tasks</h3>
-      <ul>
-        {tasks?.length > 0 &&
-          tasks.map(({ title, description, id, completed }) => {
-            return (
-              <li key={id}>
-                <p>Title: {title}</p>
-                <p>Description: {description}</p>
-                <input
-                  type="checkbox"
-                  name="completed"
-                  checked={completed}
-                  onChange={() => toggleComplete(id, !completed)}
-                />
-                <button onClick={() => deleteTask(id)}>X</button>
-                <button onClick={() => dispatch(openModalEditTask())}>
-                  Edit
-                </button>
-              </li>
-            );
-          })}
-      </ul>
-    </Container>
+    <>
+      <Container>
+        <h3>Daily tasks</h3>
+        <ul>
+          {tasks?.length > 0 &&
+            tasks.map(task => {
+              return (
+                <li key={task.id}>
+                  <p>Title: {task.title}</p>
+                  <p>Description: {task.description}</p>
+                  <input
+                    type="checkbox"
+                    name="completed"
+                    checked={task.completed}
+                    onChange={() => toggleComplete(task.id, !task.completed)}
+                  />
+                  <button onClick={() => deleteTask(task.id)}>X</button>
+                  <button onClick={() => dispatch(openModalEditTask())}>
+                    Edit
+                  </button>
+                  {isModalOpen && (
+                    <ModalContainer>
+                      <ModalEditTask task={task} />
+                    </ModalContainer>
+                  )}
+                </li>
+              );
+            })}
+        </ul>
+      </Container>
+    </>
   );
+  // return (
+  //   <>
+  //     <Container>
+  //       <h3>Daily tasks</h3>
+  //       <ul>
+  //         {tasks?.length > 0 &&
+  //           tasks.map(({ title, description, id, completed }) => {
+  //             return (
+  //               <li key={id}>
+  //                 <p>Title: {title}</p>
+  //                 <p>Description: {description}</p>
+  //                 <input
+  //                   type="checkbox"
+  //                   name="completed"
+  //                   checked={completed}
+  //                   onChange={() => toggleComplete(id, !completed)}
+  //                 />
+  //                 <button onClick={() => deleteTask(id)}>X</button>
+  //                 <button onClick={() => dispatch(openModalEditTask())}>
+  //                   Edit
+  //                 </button>
+  //                 {isModalOpen && (
+  //                   <ModalContainer>
+  //                     <ModalEditTask />
+  //                   </ModalContainer>
+  //                 )}
+  //               </li>
+  //             );
+  //           })}
+  //       </ul>
+  //     </Container>
+
+  //   </>
+  // );
 };
 
 export default TasksPage;

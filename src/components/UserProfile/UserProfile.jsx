5ffe5d03418d34fe659b94150b1acff96861c350
Lina Budkova <linabudkova35@gmail.com>
2023-04-12 15:42:28 +0300

    add user prof

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -1,14 +1,32 @@
 import useAuth from 'hooks/useAuth';
 import React from 'react';
+import brush from '../../img/brush.png';
+import {
+  BottomBox,
+  Box,
+  Img,
+  Signature,
+  Title,
+  TopBox,
+} from './UserProfile.styled';
 
 const UserProfile = () => {
   const { currentUser } = useAuth();
 
   return (
-    <div>
-      <img src={currentUser?.photoURL} alt="user" width={50} />
-      <p>{currentUser.displayName}</p>
-    </div>
+    <Box>
+      <TopBox>
+        <Title>My Profile</Title>
+        <button>
+          <img src={brush} alt="brush" />
+        </button>
+      </TopBox>
+      <BottomBox>
+        <Img src={currentUser?.photoURL} alt="user" width={50} />
+        <Signature>{currentUser.displayName}</Signature>
+        <Signature>My mood</Signature>
+      </BottomBox>
+    </Box>
   );
 };
 
